9676005d9f4b02f20c4532d7a06f980b06f361ae
Machux133 <116109655+Machux133@users.noreply.github.com>
2025-04-28 22:27:39 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const app = express();
+const server = app.listen(3000);
+const io = socketIO(server);
+
+let contestants = [];
+let buzzerLocked = true;
+let buzzerAvailable = true;
+let currentWinner = null;
+
+app.use(express.static('public'));
+console.log('Server running at http://localhost:3000/');
+console.log('for admin access, go to http://localhost:3000/admin.html');
+console.log('Press Ctrl+C to stop the server.');
+
+io.on('connection', (socket) => {
+  socket.emit('stateUpdate', { contestants, buzzerLocked, buzzerAvailable, currentWinner });
+
+  socket.on('addContestant', (name) => {
+    if (!contestants.some(c => c.name === name)) {
+      contestants.push({ name, points: 0 });
+    }
+    io.emit('stateUpdate', { contestants, buzzerLocked, buzzerAvailable, currentWinner });
+  });
+
+  socket.on('buzz', (name) => {
+    if (!contestants.some(c => c.name === name)) {
+      socket.emit('invalidName');
+      return;
+    }
+
+    if (buzzerAvailable && !buzzerLocked) {
+      currentWinner = name;
+      buzzerAvailable = false;
+      buzzerLocked = true;
+      io.emit('stateUpdate', { contestants, buzzerLocked, buzzerAvailable, currentWinner });
+    }
+  });
+
+  socket.on('resetBuzzer', () => {
+    buzzerAvailable = true;
+    currentWinner = null;
+    io.emit('stateUpdate', { contestants, buzzerLocked, buzzerAvailable, currentWinner });
+  });
+
+  socket.on('toggleLock', (locked) => {
+    buzzerLocked = locked;
+    io.emit('stateUpdate', { contestants, buzzerLocked, buzzerAvailable, currentWinner });
+  });
+
+  socket.on('adjustPoints', ({name, amount}) => {
+    const contestant = contestants.find(c => c.name === name);
+    if (contestant) contestant.points += amount;
+    io.emit('stateUpdate', { contestants, buzzerLocked, buzzerAvailable, currentWinner });
+  });
+});
